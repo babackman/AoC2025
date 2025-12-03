65adf64ad34933c6ee06edb27e49672bd4507a9d
Bruce Backman <Bruce.Backman@msidata.com>
2025-12-03 15:56:18 -0600

    day3 part 2 complete

--- Day03.java
+++ Day03.java
@@ -2,42 +2,56 @@ import java.util.List;
 
 public class Day03 {
     public static void Run(List<String> input)  {
-        part1(input);
+        //part1(input);
+        part2(input);
     }
     
     public static void part1(List<String> input)
     {
-        long totalJoltage = 0L;
+        Long totalJoltage = 0L;
         for(String line:input){
-            long bankJoltage = MaxBankJoltage(line);
+            Long bankJoltage = MaxBankJoltage(line, 2);
             totalJoltage += bankJoltage;
         }
         
         System.out.println("03.1: "+Long.toUnsignedString(totalJoltage));
     }
-    public static long MaxBankJoltage(String batteryJoltages){
-        char firstDigit=batteryJoltages.charAt(0);
-        int firstDigitIndex=0;
-        for(int i=1; i<batteryJoltages.length()-1; i++){
-            if (batteryJoltages.charAt(i)>firstDigit){
-                firstDigit=batteryJoltages.charAt(i);
-                firstDigitIndex=i;
-                if (firstDigit=='9'){
-                    break; // can't do better than 9
-                }
-            }
+    
+    public static void part2(List<String> input)
+    {
+        Long totalJoltage = 0L;
+        for(String line:input){
+            Long bankJoltage = MaxBankJoltage(line, 12);
+            totalJoltage += bankJoltage;
         }
         
-        char secondDigit=batteryJoltages.charAt(firstDigitIndex+1);
-        for(int i =firstDigitIndex+1; i<batteryJoltages.length(); i++){
-            if (batteryJoltages.charAt(i)>secondDigit){
-                secondDigit=batteryJoltages.charAt(i);
-                if (secondDigit=='9'){
+        System.out.println("03.2: "+Long.toUnsignedString(totalJoltage));
+    }
+
+    public static Long MaxBankJoltage(String batteryJoltages, int maxDigits){
+        if (maxDigits >= batteryJoltages.length()){
+            return Long.parseLong(batteryJoltages);
+        }
+        
+        String bankJoltage="";
+        int previousDigitIndex=-1;// index where previous digit was selected
+        // digitPlace is the place value of the next digit to select (0-based index from right)
+        for(int digitPlace = maxDigits-1; digitPlace >= 0; digitPlace--){
+            int digitIndex = previousDigitIndex+1;
+            char nextDigit=batteryJoltages.charAt(digitIndex);
+            for (int i=previousDigitIndex+1; i<batteryJoltages.length()-digitPlace; i++){
+                if (nextDigit=='9'){
                     break; // can't do better than 9
                 }
+                if (batteryJoltages.charAt(i)>nextDigit){
+                    nextDigit=batteryJoltages.charAt(i);
+                    digitIndex=i;
+                }
             }
+            previousDigitIndex=digitIndex;
+            bankJoltage += nextDigit;
         }
-        String bankJoltage=""+firstDigit+secondDigit;
-        return Long.parseLong(bankJoltage);
+
+        return Long.parseUnsignedLong(bankJoltage);
     }
 }
