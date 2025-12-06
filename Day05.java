a680d94a793198d3646ac7c196a1c253c1f18688
Bruce Backman <Bruce.Backman@msidata.com>
2025-12-06 12:05:16 -0600

    Day 5 part 2 naiive implmentation, too slow

--- Day05.java
+++ Day05.java
@@ -6,6 +6,7 @@ public class Day05 {
     public static void Run(List<String> input) {
         IngredientDb db = new IngredientDb(input);
         part1(db);
+        part2(db);
     }
     
     private static void part1(IngredientDb db) {
@@ -31,6 +32,37 @@ public class Day05 {
         System.out.println("5.1: fresh count: " + freshCount);
     }
 
+    private static void part2(IngredientDb db) {
+        long freshCount = 0;
+        for (int i=0; i < db.freshRanges.size(); i++) {
+            IdRange range = db.freshRanges.get(i);
+            if (i==0) {
+                freshCount = range.max - range.min + 1;
+                continue;
+            }
+            for (long ingredient=range.min; ingredient <= range.max; ingredient++) {
+                // check if the ingredient is in any of the previous ranges
+                boolean inPreviousRange = false;
+                for (int prevIndex = 0; prevIndex < i; prevIndex++) {
+                    IdRange prevRange = db.freshRanges.get(prevIndex);
+                    if (Long.compareUnsigned(ingredient, prevRange.min) < 0) {
+                        // ingredient is less than the minimum of this range, so it can't be in any
+                        // further ranges
+                        break;
+                    }
+                    if (Long.compareUnsigned(ingredient, prevRange.max) <= 0) {
+                        // ingredient is in this range
+                        inPreviousRange = true;
+                        break;
+                    }
+                }
+                if (!inPreviousRange) {
+                    freshCount++;
+                }
+            }
+        }  
+        System.out.println("5.2: fresh count: " + freshCount);
+    }
     private static class IngredientDb {
         public List<IdRange> freshRanges;
         public long[] ingredients;
