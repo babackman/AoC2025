392924de8ce483bf397224cc734c09e0cec0aaea
Bruce Backman <Bruce.Backman@msidata.com>
2025-12-06 11:06:13 -0600

    Day 6 part 2 complete

--- Day06.java
+++ Day06.java
@@ -2,29 +2,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Day06 {
-    private List<MathProblem> _mathProblems;
-    
     public static void Run(List<String> input) {
-        Day06 day06 = new Day06(input);
-        day06.part1();
-        // day06.part2();
+        part1(input);
+        part2(input);
     }
 
-    public Day06(List<String> input) {
-        super();
-        _mathProblems = ReadMathProblems(input);
-    }
-    
-    private void part1() {
+    private static void part1(List<String> input) {
+        List<MathProblem> mathProblems = ReadMathProblems(input);
         Long totalSum = 0L;
-        for (MathProblem problem : _mathProblems) {
+        for (MathProblem problem : mathProblems) {
             totalSum += problem.Evaluate();
         }
         System.out.println("06.1: " + Long.toUnsignedString(totalSum));
     }
+
+    private static void part2(List<String> input) {
+        List<MathProblem> mathProblems = ReadCephalopodMathProblems(input);
+        Long totalSum = 0L;
+        for (MathProblem problem : mathProblems) {
+            totalSum += problem.Evaluate();
+        }
+        System.out.println("06.2: " + Long.toUnsignedString(totalSum));
+    }
     
-    private List<MathProblem> ReadMathProblems(List<String> input) {
-        List<MathProblem> problems = new ArrayList<MathProblem>();
+    private static List<MathProblem> ReadMathProblems(List<String> input) {
+        List<MathProblem> problems;
         int numOperands =input.size() -1;
         
         String inputLine = input.get(numOperands);
@@ -46,7 +48,42 @@ public class Day06 {
         }
         return problems;
     }
-    class MathProblem {
+    
+    private static List<MathProblem> ReadCephalopodMathProblems(List<String> input) {
+        // read operands from columns, not tokens
+
+        // positions of operators mark the leading edges of problems
+        ArrayList<Integer> operatorPositions = new ArrayList<Integer>();
+        String operandLine = input.get(input.size() - 1);
+        for (int i = 0; i < operandLine.length(); i++) {
+            if (operandLine.charAt(i) != ' ') {
+                operatorPositions.add(i);
+            }
+        }
+        ArrayList<MathProblem> problems = new ArrayList<MathProblem>(operatorPositions.size());
+
+        for (int problemIndex = 0; problemIndex < operatorPositions.size(); problemIndex++) {
+            int startIndex = operatorPositions.get(problemIndex);
+            char operator = operandLine.charAt(startIndex);
+            int endIndex = (problemIndex == (operatorPositions.size() - 1))
+                    ? operandLine.length() - 1
+                    : operatorPositions.get(problemIndex + 1) - 2;
+            MathProblem problem = new MathProblem(operator, endIndex - startIndex + 1);
+            for (int column = endIndex; column >= startIndex; column--) {
+                String operandString = "";
+                for (int row = 0; row < input.size() - 1; row++) {
+                    char ch = input.get(row).charAt(column);
+                    if (ch != ' ')
+                        operandString = operandString + ch;
+                }
+                problem.AddOperand(Long.parseLong(operandString));
+            }
+            problems.add(problem);
+        }
+        return problems;
+    }
+    
+    static class MathProblem {
         ArrayList<Long> _operands;
         char _operator;
         
